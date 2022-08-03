a2d230fef4733e1cafc00b8a68b32ff34255054a
Giovana do Nascimento <giovana.np1@gmail.com>
2022-08-03 17:02:28 -0300

    adjust sass

--- src/components/Contato/Contato.js
+++ src/components/Contato/Contato.js
@@ -14,7 +14,7 @@ function Contato() {
         </Col>
         <Col sm={6} className="icon-contato d-flex justify-content-center flex-column">
             <h1>CONTATO</h1>
-            <a href="https://www.linkedin.com/in/giovana-do-nascimento/" ><img src='https://cdn-icons.flaticon.com/png/512/3536/premium/3536569.png?token=exp=1659488426~hmac=1b8636d62ce5c10ccd1812a4fb372fb3' alt="Linkedin" />https://www.linkedin.com/in/giovana-do-nascimento/</a>
+            <a href="https://www.linkedin.com/in/giovana-do-nascimento/" ><img src='https://cdn-icons-png.flaticon.com/512/254/254394.png' alt="Linkedin" />https://www.linkedin.com/in/giovana-do-nascimento/</a>
             <a href="https://www.instagram.com/giovananp/"><img src='https://cdn-icons-png.flaticon.com/512/1077/1077042.png' alt="Instagram" />@giovananp</a>
             <a href="mailto:giovana.np1@gmail.com"><img src='https://cdn-icons-png.flaticon.com/512/561/561127.png' alt="Email" />giovana.np1@gmail.com</a>
 
--- src/components/Principal/Principal.js
+++ src/components/Principal/Principal.js
@@ -4,18 +4,18 @@ import Col from 'react-bootstrap/Col';
 import photo from '../../utils/image/foto.jpg'
 
 function Principal() {
-  return (
-    <Container className="principal" id="principal">
-      <Row className="d-flex justify-content-around">
-        <Col sm={6} className="d-flex justify-content-center flex-column">
-            <h1>GIOVANA DO NASCIMENTO PENA</h1>
-            <h2>FULLSTACK DEVELOPER</h2>
-        </Col>
-        <Col sm={6}className="photo-principal d-flex justify-content-around">
-            <img src={photo} alt="Logo" />
-        </Col>
-      </Row>
-    </Container>
+    return (
+        <Container className="principal" id="principal">
+        <Row className="d-flex justify-content-around">
+            <Col sm={6} className="d-flex justify-content-center flex-column">
+                <h1>GIOVANA DO NASCIMENTO PENA</h1>
+                <h2>FULLSTACK DEVELOPER</h2>
+            </Col>
+            <Col sm={6}className="photo-principal d-flex justify-content-around">
+                <img src={photo} alt="Logo" />
+            </Col>
+        </Row>
+        </Container>
   );
 }
 
